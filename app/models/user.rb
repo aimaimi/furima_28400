201ea6c8132131eca9271159652d7c31fbb42a5a
aimaimi <yh1127@hotmail.co.jp>
2020-08-10 17:10:49 +0900

    正規表現を変数で表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
+  katakana = /\A[ァ-ン]+\z/
+  alphanumeric = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/
+
   validates :nickname,     presence: true
 
-  validates :password,     format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "パスワードには英字と数字を混ぜてください"}
+  validates :password,     format: { with: alphanumeric, message: "パスワードには英字と数字を混ぜてください"}
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角文字を使用してください" } do
+  with_options presence: true, format: { with: zenkaku, message: "全角文字を使用してください" } do
     validates :second_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: "全角カタカナを使用してください"} do
+  with_options presence: true, format: { with: katakana, message: "全角カタカナを使用してください"} do
     validates :second_kana
     validates :first_kana
   end
