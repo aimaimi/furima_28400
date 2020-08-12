3599ae8bef2b610326f19a9aa5976ed9ed638914
aimaimi <yh1127@hotmail.co.jp>
2020-08-12 19:17:35 +0900

    価格による手数料、利益の非同期通信実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,8 +3,8 @@ window.addEventListener('load', function(){
   const fee = document.getElementById("add-tax-price")
   const gain = document.getElementById("profit")
 
-  priceForm.addEventListener('click', function (){
-    fee.innerHTML = priceForm.innerHTML / 10 ;
-    gain.innerHTML = priceForm.innerHTML - fee.innerHTML ;
+  priceForm.addEventListener('change', function (){
+    fee.innerHTML = Math.floor(this.value / 10);
+    gain.innerHTML = this.value - fee.innerHTML;
   })
 })
