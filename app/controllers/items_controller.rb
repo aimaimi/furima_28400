13790a00165e1206cd77b3df09ce416632f4c6cc
aimaimi <yh1127@hotmail.co.jp>
2020-08-14 14:50:36 +0900

    他の商品情報が変更されない機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if Item.update(item_params)
+    if Item.find(params[:id]).update(item_params)
       redirect_to item_path
     else
       render 'edit'
