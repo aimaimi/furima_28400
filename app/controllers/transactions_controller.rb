e973ff1f6909f29689bd308dcd4be07c478e0aa9
aimaimi <yh1127@hotmail.co.jp>
2020-08-18 15:49:30 +0900

    クレジット決済機能実装

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,33 @@
 class TransactionsController < ApplicationController
   def index
-     @item = Item.find(params[:format])
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @orderitem = OrderItem.new
   end
 
   def create
+    @orderitem = OrderItem.new(postal_code: transaction_params[:postal_code],prefecture_id: transaction_params[:prefecture_id],city: transaction_params[:city],address: transaction_params[:address],building: transaction_params[:building],phone_number: transaction_params[:phone_number],item_id: transaction_params[:item_id],user_id: current_user.id)
+    if @orderitem.save
+      pay_item
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def transaction_params
+    params.permit(:token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_d5bf67e4686670183742b583"
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: transaction_params[:token],
+      currency: 'jpy'
+    )
   end
 end
