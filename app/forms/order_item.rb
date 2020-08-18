9e5ab7465e2ea1c2a09a5f7037b87583242829b0
aimaimi <yh1127@hotmail.co.jp>
2020-08-18 17:05:52 +0900

    バリデーションの設定

--- app/forms/order_item.rb
+++ app/forms/order_item.rb
@@ -3,7 +3,24 @@ class OrderItem
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
 
+  include_hyphen = /\A\d{3}[-]\d{4}\z/
+  only_number = /\A\d{,11}\z/
+  card_number = /\A\d{14,16}\z/
+  month_year = /\A\d{2}\z/
+  cvc_number = /\A\d{3,4}\z/
+
+  validates :postal_code, format: {with: include_hyphen, message: "is invalid.Include hyphen(-)"}
+  validates :phone_number, format: {with: only_number, message: "is invalid.Don't include hyphen(-)"}
+  validates :number, format: {with: card_number}
+  validates :exp_month, format: {with: month_year}
+  validates :exp_year, format: {with: month_year}
+  validates :cvc, format: {with: cvc_number}
+
   with_options presence: true do
+    validates :number
+    validates :exp_month
+    validates :exp_year
+    validates :cvc
     validates :postal_code
     validates :prefecture_id
     validates :city
