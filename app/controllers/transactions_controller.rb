ec95c53a29ed06a80e6b3862e1038409f626a82a
aimaimi <yh1127@hotmail.co.jp>
2020-08-19 10:30:18 +0900

    rubocopによる修正

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -27,7 +27,7 @@ class TransactionsController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: Item.find(params[:item_id]).price,
       card: transaction_params[:token],
--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_item do
-    postal_code { "123-4567"}
+    postal_code { '123-4567' }
     prefecture_id { 2 }
-    city { "豊島区" }
-    address { "池袋" }
-    building { "柳" }
-    phone_number { "09012345678" }
+    city { '豊島区' }
+    address { '池袋' }
+    building { '柳' }
+    phone_number { '09012345678' }
     item_id { 12 }
     user_id { 1 }
   end
